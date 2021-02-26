c942cc27dc4a49ce941f815cd0cbca0a677e21a1
Amit Sides <amit794@walla.com>
2021-02-26 09:51:53 -0800

    WolfSSL client POC

--- Project/Client/main.cpp
+++ Project/Client/main.cpp
@@ -5,6 +5,7 @@
 #include <wolfssl/test.h>
 #include <errno.h>
 #define SERV_PORT 11111
+#define NO_SHA256
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
     WOLFSSL* ssl;
     WOLFSSL_METHOD* method;
     struct  sockaddr_in servAddr;
-    const char message[] = "Hello, World!";
+    const char message[] = "Hello, World!\n";
 
     /* create and set up socket */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -26,11 +27,27 @@ int main()
 
     /* initialize wolfssl library */
     wolfSSL_Init();
-    method = wolfTLSv1_2_client_method(); /* use TLS v1.2 */
+    method = wolfTLSv1_3_client_method(); /* use TLS v1.3 */
 
     /* make new ssl context */
     if ( (ctx = wolfSSL_CTX_new(method)) == NULL) {
           err_sys("wolfSSL_CTX_new error");
+    }
+
+        /* Add cert to ctx */
+#define CA "../../Server/Certificates/ca.crt"
+    system("ls " CA);
+    if (wolfSSL_CTX_load_verify_locations(ctx, CA, 0) !=
+            SSL_SUCCESS) {
+        std::cout << "Error: " << wolfSSL_CTX_load_verify_locations(ctx, CA, 0) << std::endl;
+        //err_sys("Error loading ca.crt");
+    }
+
+    /* Set cipher list */
+#define CIPHER_LIST "TLS_AES_256_GCM_SHA384"
+    if (wolfSSL_CTX_set_cipher_list(ctx, CIPHER_LIST) != SSL_SUCCESS) {
+        fprintf(stderr, "ERROR: failed to set cipher list\n");
+        return -1;
     }
 
     /* make new wolfSSL struct */
@@ -38,22 +55,16 @@ int main()
          err_sys("wolfSSL_new error");
     }
 
-
-    /* Add cert to ctx */
-    system("ls ../../Server/Certificates");
-    if (wolfSSL_CTX_load_verify_locations(ctx, "../../Server/Certificates/ca.pem", NULL) !=
-            SSL_SUCCESS) {
-        std::cout << "Error: " << wolfSSL_CTX_load_verify_locations(ctx, NULL, "../../Server/Certificates") << std::endl;
-        //err_sys("Error loading ca.crt");
-    }
-
     /* Connect wolfssl to the socket, server, then send message */
     wolfSSL_set_fd(ssl, sockfd);
     wolfSSL_connect(ssl);
     wolfSSL_write(ssl, message, strlen(message));
+    wolfSSL_shutdown(ssl);
 
     /* frees all data before client termination */
     wolfSSL_free(ssl);
     wolfSSL_CTX_free(ctx);
     wolfSSL_Cleanup();
+
+    return 99;
 }
