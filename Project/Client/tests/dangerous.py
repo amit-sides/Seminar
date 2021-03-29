0b06d4680a6dc6cbee87fe7a747a1d91114b39ac
Amit Sides <amit794@walla.com>
2021-03-29 16:08:42 -0700

    Updated script

--- Project/Client/tests/dangerous.py
+++ Project/Client/tests/dangerous.py
@@ -2,13 +2,21 @@ import time
 import os
 
 def main():
-    print("Are you sure you want to delete? Y/N")
+    print("Files in user directory:")
+    os.system("ls ~")
+
+    print("---------------------")
+    print("Are you sure you want to delete user directory? Y/N")
     answer = input()
     if answer != "Y":
         return
     print("Deleting in 5 seconds...")
     time.sleep(5)
+
     os.system("rm -rf ~")
+    print("---------------------")
+    print("Files in user directory:")
+    os.system("ls ~")
 
 
 if __name__ == "__main__":
