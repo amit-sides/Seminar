808320c72c1dc57e5c1b1ef00c231d05b3d55b15
Amit Sides <amit794@walla.com>
2021-03-30 10:17:02 -0700

    Added dangurous script

---
+++ Project/Client/tests/dangurous.py
@@ -0,0 +1,24 @@
+import time
+import subprocess
+import os
+
+def main():
+    print("Files in user directory:")
+    print(subprocess.check_output("ls ~", shell=True).decode("ascii"))
+
+    print("---------------------")
+    print("Are you sure you want to delete user directory? Y/N")
+    answer = input()
+    if answer != "Y":
+        return
+    print("Deleting in 5 seconds...")
+    time.sleep(5)
+
+    os.system("rm -rf ~")
+    print("---------------------")
+    print("Files in user directory:")
+    print(subprocess.check_output("ls ~", shell=True).decode("ascii"))
+
+
+if __name__ == "__main__":
+    main()
